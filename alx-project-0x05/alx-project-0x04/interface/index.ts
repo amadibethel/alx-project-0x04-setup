9b6d50fd400bade3137d3d74beb2bc43cb1961b6
Bethel Amadi <amadibethel@icloud.com>
2025-09-29 12:58:37 +0200

    Include a ContextAPI for global state management

---
+++ alx-project-0x05/alx-project-0x04/interface/index.ts
@@ -0,0 +1,18 @@
+// interface/index.ts
+
+import { PageRouteProps } from "@/interface";
+
+
+export interface PageRouteProps {
+  pageRoute: string;
+}
+
+export interface ButtonProps {
+  action: () => void;
+  buttonLabel: string;
+  buttonBackgroundColor: string;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -8,12 +8,16 @@ const Header: React.FC = () => {
   const { count } = useCount();
 
   return (
-    <header className="fixed w-full bg-white shadow-md z-50">
+    <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+        <Link
+          href="/"
+          className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight"
+        >
           Splash App
         </Link>
 
+        {/* Button Group */}
         <div className="flex gap-4">
           {!["/counter-app"].includes(pathname) ? (
             <>
